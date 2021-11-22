c9915db25fc52499dc31946e1c13d1e448625bd4
3sulton <16520492@std.stei.itb.ac.id>
2021-11-22 15:26:31 +0700

    Update map.pl
    nambah tile buat tanaman, nambah state, nambah comment

--- map.pl
+++ map.pl
@@ -4,13 +4,28 @@
 % pojok kanan bawah adalah (10, 15)
 % pagar ada di (0, Ry), (11, Ry) dan (Rx, 0), (Rx, 16) dengan 0<=Ry<=16 dan 0<=Rx<=11
 
-:- dynamic(playerPoint/2).
-:- dynamic(diggedTilePoint/2).
-:- dynamic(playerCell/1).
+% sementara
+state(free).
 
+:- dynamic(playerPoint/2).
+:- dynamic(playerCell/1).
+% playerCell dapat berupa:
+% 'H' : house
+% 'R' : ranch
+% 'M' : marketplace
+% 'Q' : quest
+% 'o' : di sekitar water tile
+% '=' : digged tile
+% 'p' : potato
+% 'c' : carrot
+% 't' : tomato
+% 'r' : rice
+
+% awal permainan, pemain akan berada di rumah
 playerCell('H').
 playerPoint(5, 8).
 
+% koordinat lokasi
 nRow(11).
 nCol(16).
 housePoint(5, 8).
@@ -30,6 +45,14 @@ waterTilePoint(8, 10).
 fence(R, _) :- R = 0; nRow(R).
 fence(_, C) :- C = 0; nCol(C).
 
+% farming
+:- dynamic(diggedTilePoint/2).
+:- dynamic(potatoTilePoint/2).
+:- dynamic(carrotTilePoint/2).
+:- dynamic(tomatoTilePoint/2).
+:- dynamic(riceTilePoint/2).
+
+% printMap
 printCell(R, C) :-
     fence(R, C),
     !,
@@ -62,9 +85,29 @@ printCell(R, C) :-
     diggedTilePoint(R, C),
     !,
     write('=').
+printCell(R, C) :-
+    potatoTilePoint(R, C),
+    !,
+    write('p').
+printCell(R, C) :-
+    carrotTilePoint(R, C),
+    !,
+    write('c').
+printCell(R, C) :-
+    tomatoTilePoint(R, C),
+    !,
+    write('t').
+printCell(R, C) :-
+    riceTilePoint(R, C),
+    !,
+    write('r').
 printCell(_,_) :-
 	write('-').
 
+map :-
+  state(S),
+  S = not_started, !,
+  write('Permainan belum dimulai.\n').
 map :-
     nRow(R),
 	nCol(C),
@@ -75,20 +118,23 @@ map :-
 		nl
 	)).
 
+% exploration mechanism
+w :-
+  state(S),
+  S = not_started, !,
+  write('Permainan belum dimulai.\n').
 w :-
     playerPoint(R, C),
     NewR is R - 1,
     fence(NewR, C),
     !, 
     write('Kamu mencoba meloncati pagar di utara tapi kamu tersangkut').
-
 w :-
     playerPoint(R, C),
     NewR is R - 1,
     waterTilePoint(NewR, C),
     !, 
     write('Kamu enggak boleh nyebur ke sungai karena kamu tidak bisa berenang').
-
 w :-
     playerPoint(R, C),
     NewR is R - 1,
@@ -96,22 +142,25 @@ w :-
     asserta(playerPoint(NewR, C)),
     !,
     map,
+    write('Kamu berhasil bergerak ke utara'), nl,
     cellCheck(NewR, C).
 
+s :-
+  state(S),
+  S = not_started, !,
+  write('Permainan belum dimulai.\n').
 s :-
     playerPoint(R, C),
     NewR is R + 1,
     fence(NewR, C),
     !, 
     write('Kamu mencoba meloncati pagar di selatan tapi kamu tersangkut').
-
 s :-
     playerPoint(R, C),
     NewR is R + 1,
     waterTilePoint(NewR, C),
     !, 
     write('Kamu enggak boleh nyebur ke sungai karena kamu tidak bisa berenang').
-
 s :-
     playerPoint(R, C),
     NewR is R + 1,
@@ -119,22 +168,25 @@ s :-
     asserta(playerPoint(NewR, C)),
     !,
     map,
+    write('Kamu berhasil bergerak ke selatan'), nl,
     cellCheck(NewR, C).
 
+a :-
+  state(S),
+  S = not_started, !,
+  write('Permainan belum dimulai.\n').
 a :-
     playerPoint(R, C),
     NewC is C - 1,
     fence(R, NewC),
     !, 
     write('Kamu mencoba meloncati pagar di barat tapi kamu tersangkut').
-
 a :-
     playerPoint(R, C),
     NewC is C - 1,
     waterTilePoint(R, NewC),
     !, 
     write('Kamu enggak boleh nyebur ke sungai karena kamu tidak bisa berenang').
-
 a :-
     playerPoint(R, C),
     NewC is C - 1,
@@ -142,22 +194,25 @@ a :-
     asserta(playerPoint(R, NewC)),
     !,
     map,
+    write('Kamu berhasil bergerak ke barat'), nl,
     cellCheck(R, NewC).
 
+d :-
+  state(S),
+  S = not_started, !,
+  write('Permainan belum dimulai.\n').
 d :-
     playerPoint(R, C),
     NewC is C + 1,
     fence(R, NewC),
     !, 
     write('Kamu mencoba meloncati pagar di timur tapi kamu tersangkut').
-
 d :-
     playerPoint(R, C),
     NewC is C + 1,
     waterTilePoint(R, NewC),
     !, 
     write('Kamu enggak boleh nyebur ke sungai karena kamu tidak bisa berenang').
-
 d :-
     playerPoint(R, C),
     NewC is C + 1,
@@ -165,36 +220,34 @@ d :-
     asserta(playerPoint(R, NewC)),
     !,
     map,
+    write('Kamu berhasil bergerak ke timur'), nl,
     cellCheck(R, NewC).
 
+% pengecekan cell
 cellCheck(R, C) :-
     housePoint(R, C),
     retractall(playerCell(_)),
     asserta(playerCell('H')),
     !,
     write('Kamu sekarang ada di rumah').
-
 cellCheck(R, C) :-
     ranchPoint(R, C),
     retractall(playerCell(_)),
     asserta(playerCell('R')),
     !,
     write('Kamu sekarang ada di peternakan').
-
 cellCheck(R, C) :-
     marketplacePoint(R, C),
     retractall(playerCell(_)),
     asserta(playerCell('M')),
     !,
     write('Kamu sekarang ada di pasar').
-
 cellCheck(R, C) :-
     questPoint(R, C),
     retractall(playerCell(_)),
     asserta(playerCell('Q')),
     !,
     write('Kamu sekarang ada di tempat pengambilan quest').
-
 cellCheck(R, C) :-
     waterTilePoint(Rw, Cw),
     (
@@ -207,14 +260,36 @@ cellCheck(R, C) :-
     asserta(playerCell('o')),
     !,
     write('Kamu sekarang ada di sekitar danau').
-
 cellCheck(R, C) :-
     diggedTilePoint(R, C),
     retractall(playerCell(_)),
     asserta(playerCell('=')),
     !,
     write('Kamu sekarang ada di tanah yang sedang digali').
-
+cellCheck(R, C) :-
+    potatoTilePoint(R, C),
+    retractall(playerCell(_)),
+    asserta(playerCell('p')),
+    !,
+    write('Kamu sekarang ada di kebun kentang').
+cellCheck(R, C) :-
+    carrotTilePoint(R, C),
+    retractall(playerCell(_)),
+    asserta(playerCell('c')),
+    !,
+    write('Kamu sekarang ada di kebun wortel').
+cellCheck(R, C) :-
+    tomatoTilePoint(R, C),
+    retractall(playerCell(_)),
+    asserta(playerCell('t')),
+    !,
+    write('Kamu sekarang ada di kebun tomat').
+cellCheck(R, C) :-
+    diggedTilePoint(R, C),
+    retractall(playerCell(_)),
+    asserta(playerCell('r')),
+    !,
+    write('Kamu sekarang ada di sawah padi').
 cellCheck(_, _) :-
     retractall(playerCell(_)),
     asserta(playerCell('-')).
