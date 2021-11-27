f6167287b8406551b1d1add1cf29c06516b5f9a7
3sulton <16520492@std.stei.itb.ac.id>
2021-11-27 17:03:11 +0700

    create buyable fact

--- item.pl
+++ item.pl
@@ -1,12 +1,13 @@
-
-
-
+% farming
 item(potato_seed,farming).
 item(carrot_seed,farming).
 item(tomato_seed,farming).
 item(rice_seed,farming).
 item(potato,farming).
 item(carrot,farming).
+item(tomato,farming).
+item(rice,farming).
+% ranching
 item(chicken,ranching).
 item(sheep,ranching).
 item(cow,ranching).
@@ -14,7 +15,7 @@ item(turkey,ranching).
 item(egg,ranching).
 item(wol,ranching).
 item(milk,ranching).
-item(bait,fishing).
+% fishing
 item(tuna,fishing).
 item(nila,fishing).
 item(lele,fishing).
@@ -23,23 +24,47 @@ item(patin,fishing).
 item(barracuda,fishing).
 item(hiu,fishing).
 item(pari,fishing).
-item(shovel,farming).
+% tool
+item(bait,fishing).
 item(fishing_rod,fishing).
 item(good_rod,fishing).
 item(rare_rod,fishing).
 item(legend_rod,fishing).
+item(shovel,farming).
 item(good_fertilizer,farming).
 item(best_fertilizer,farming).
 item(instant_fertilizer,farming).
 
+% yang bisa dibeli
+% kalau dijual jadi 50% harga beli
+buyable(potato_seed).
+buyable(carrot_seed).
+buyable(tomato_seed).
+buyable(rice_seed).
+buyable(chicken).
+buyable(sheep).
+buyable(cow).
+buyable(turkey).
+buyable(bait).
+buyable(fishing_rod).
+buyable(good_rod).
+buyable(rare_rod).
+buyable(legend_rod).
+buyable(shovel).
+buyable(good_fertilizer).
+buyable(best_fertilizer).
+buyable(instant_fertilizer).
 
-
+% farming
 itemPrice(potato_seed,40).
 itemPrice(carrot_seed,60).
 itemPrice(tomato_seed,50).
-itemPrice(rice_seed,40).
+itemPrice(rice_seed,70).
 itemPrice(potato,80).
-itemPrice(carrot,100).
+itemPrice(carrot,120).
+itemPrice(tomato,100).
+itemPrice(rice,150).
+% ranching
 itemPrice(chicken,200).
 itemPrice(sheep,1500).
 itemPrice(cow,2000).
@@ -47,9 +72,8 @@ itemPrice(turkey,750).
 itemPrice(egg,50).
 itemPrice(wol,200).
 itemPrice(milk,50).
+% fishing
 itemPrice(tuna,200).
-itemPrice(tuna,175).
-itemPrice(bait,5).
 itemPrice(nila,50).
 itemPrice(lele,30).
 itemPrice(gurame,80).
@@ -57,11 +81,13 @@ itemPrice(patin,80).
 itemPrice(barracuda,7500).
 itemPrice(hiu,10000).
 itemPrice(pari,1000).
-itemPrice(shovel,300).
+% tool
+itemPrice(bait,5).
 itemPrice(fishing_rod,500).
 itemPrice(good_rod,2000).
 itemPrice(rare_rod,4000).
 itemPrice(legend_rod,10000).
+itemPrice(shovel,300).
 itemPrice(good_fertilizer,40).
 itemPrice(best_fertilizer,70).
 itemPrice(instant_fertilizer,100).
